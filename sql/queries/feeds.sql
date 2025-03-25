2e1cb9e4c1c4ad5b935f7b8afb75f6433899bedb
Lamont Banks <lamontbanks@gmail.com>
2025-03-25 13:42:37 -0400

    Feeds database, and query
    user+url must be unique

---
+++ sql/queries/feeds.sql
@@ -0,0 +1,13 @@
+-- https://docs.sqlc.dev/en/latest/tutorials/getting-started-postgresql.html
+
+-- name: CreateFeed :one
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6
+)
+RETURNING *;
--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -6,10 +6,14 @@ CREATE TABLE feeds (
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name TEXT NOT NULL DEFAULT 'Unspecified',
-    url TEXT UNIQUE,
+    url TEXT NOT NULL DEFAULT 'Unspecified',
     -- PostgresSQL Foreign Keys: https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-FK
     -- REFERENCE automatically uses the PRIMARY KEY of the referenced table
-    user_id uuid REFERENCES users  
+    user_id uuid NOT NULL REFERENCES users,
+    -- UNIQUE CONSTRAINT for url and user_id:
+    -- a user can only add the same feed once, but multiple users can have the same feed
+    -- https://www.postgresql.org/docs/17/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
+    UNIQUE (url, user_id)
 );
 
 -- +goose Down
