a7e2cad3c57718e54857c8a9c1580c9fc60eb65f
Lamont Banks <lamontbanks@gmail.com>
2025-03-25 16:37:06 -0400

    Revert `feeds.url` to `UNIQUE` - will implement a JOIN table instead

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -11,17 +11,14 @@ CREATE TABLE feeds (
     name TEXT       NOT NULL 
                     DEFAULT 'Unspecified',
     url TEXT        NOT NULL 
+                    UNIQUE
                     DEFAULT 'Unspecified',
     -- PostgresSQL Foreign Keys: https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-FK
     -- REFERENCE automatically uses the PRIMARY KEY of the referenced table
     user_id uuid    NOT NULL 
                     REFERENCES users
                     -- DELETES this row in 'feeds' if user_id in 'users' is deleted
-                    ON DELETE CASCADE,
-    -- UNIQUE CONSTRAINT for url and user_id:
-    -- a user can only add the same feed once, but multiple users can have the same feed
-    -- https://www.postgresql.org/docs/17/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
-    UNIQUE (url, user_id)
+                    ON DELETE CASCADE
 );
 
 -- +goose Down
