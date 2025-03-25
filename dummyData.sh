20c27161e1c5b5f55868847e788be3b1f39dd242
Lamont Banks <lamontbanks@gmail.com>
2025-03-25 14:48:58 -0400

    Script to populate test data

---
+++ dummyData.sh
@@ -0,0 +1,22 @@
+$/bin/zsh
+
+go run . reset
+
+go run . register alice
+go run . register bob
+go run . register cal
+
+go run . users
+
+go run . login alice
+go run . addFeed "Pivot TO AI" https://pivot-to-ai.com/feed/
+
+go run . login bob
+go run . addFeed "Pivot TO AI" https://pivot-to-ai.com/feed/
+go run . addFeed "MassivelyOP Podcast" https://feeds.libsyn.com/63150/rss
+
+go run . login bob
+go run . addFeed "MassivelyOP Podcast" https://feeds.libsyn.com/63150/rss
+go run . addFeed "MassivelyOP Podcast" https://www.guildwars2.com/en/feed/
+
+go run . feeds
