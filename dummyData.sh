91fda47c800f8539b5be1aa352256f43e68e701e
Lamont Banks <lamontbanks@gmail.com>
2025-03-25 15:07:11 -0400

    test data script fixes

--- dummyData.sh
+++ dummyData.sh
@@ -4,19 +4,16 @@ go run . reset
 
 go run . register alice
 go run . register bob
-go run . register cal
-
-go run . users
+go run . register cat
 
 go run . login alice
-go run . addFeed "Pivot TO AI" https://pivot-to-ai.com/feed/
+go run . addFeed "Pivot To AI" https://pivot-to-ai.com/feed/
 
 go run . login bob
-go run . addFeed "Pivot TO AI" https://pivot-to-ai.com/feed/
+go run . addFeed "Pivot To AI" https://pivot-to-ai.com/feed/
 go run . addFeed "MassivelyOP Podcast" https://feeds.libsyn.com/63150/rss
 
-go run . login bob
+go run . login cat
+go run . addFeed "Pivot To AI" https://pivot-to-ai.com/feed/
 go run . addFeed "MassivelyOP Podcast" https://feeds.libsyn.com/63150/rss
-go run . addFeed "MassivelyOP Podcast" https://www.guildwars2.com/en/feed/
-
-go run . feeds
+go run . addFeed "Guild Wars 2" https://www.guildwars2.com/en/feed/
