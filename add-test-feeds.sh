2938101323c52dcf5e0c1a953bfe8ff388aaada1
Lamont Banks <lamontbanks@gmail.com>
2025-04-08 11:03:55 -0400

    Test data scripts

---
+++ add-test-feeds.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+go run main.go feeds add -n "test rss 1" -u "www.example1.com/feed/rss.xml" 
+go run main.go feeds add -n "test rss 2" -u "www.example2.com/feed/rss.xml" 
+go run main.go feeds add -n "test rss 3" -u "www.example3.com/feed/rss.xml" 
+go run main.go feeds add -n "test rss 4" -u "www.example4.com/feed/rss.xml" 
+go run main.go feeds add -n "test rss 5" -u "www.example5.com/feed/rss.xml" 
