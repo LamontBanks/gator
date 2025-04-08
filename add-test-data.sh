d17d13bbc010f807cfb82d6d6c21e6f9fae79296
Lamont Banks <lamontbanks@gmail.com>
2025-04-08 11:35:36 -0400

    Test data typo

--- add-test-data.sh
+++ add-test-data.sh
@@ -8,7 +8,7 @@ go run main.go register Cat
 
 go run main.go login Alice
 go run main.go feeds add -n "Nasa Image of the Day" -u https://www.nasa.gov/feeds/iotd-feed/
-go run main.go feeds add -n "Pivot To AI" -u https://pivot-to-ui.com/feed/
+go run main.go feeds add -n "Pivot To AI" -u https://pivot-to-ai.com/feed/
 go run main.go feeds add -n "Guild Wars 2" -u https://www.guildwars2.com/en/feed/
 
 
