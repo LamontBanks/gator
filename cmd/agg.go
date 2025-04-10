95e19e26284338123401faadca745063809ca741
Lamont Banks <lamontbanks@gmail.com>
2025-04-09 21:35:22 -0400

    `agg` with cobra-cli
    Delete old agg file

--- cmd/agg.go
+++ cmd/agg.go
@@ -1,40 +1,115 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/LamontBanks/gator/internal/database"
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
+
+// agg commands
+var updateFreq string
 
 // aggCmd represents the agg command
 var aggCmd = &cobra.Command{
 	Use:   "agg",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Long:  ``,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		freq, err := time.ParseDuration(updateFreq)
+		if err != nil {
+			return err
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("agg called")
+		fmt.Printf("Updating feeds every %v\n", updateFreq)
+
+		ticker := time.NewTicker(freq)
+		for ; ; <-ticker.C {
+			fmt.Println("Updating RSS feeds...")
+			updateAllFeeds(appState)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(aggCmd)
 
-	// Here you will define your flags and configuration settings.
+	aggCmd.Flags().StringVarP(&updateFreq, "freq", "f", "15m", "feed update frequency")
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// aggCmd.PersistentFlags().String("foo", "", "A help for foo")
+func updateAllFeeds(s *state) error {
+	allFeeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return err
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// aggCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Update all feeds at once using goroutines
+	feedUpdatedCh := make(chan struct{})
+
+	for _, feed := range allFeeds {
+		go func() {
+			updateSingleFeed(s, feed.Url)
+			feedUpdatedCh <- struct{}{}
+		}()
+	}
+
+	// Wait for all feeds to update
+	for range allFeeds {
+		<-feedUpdatedCh
+	}
+
+	fmt.Printf("Feeds updated at %v\n", time.Now().Format("3:04PM"))
+	return nil
+}
+
+func updateSingleFeed(s *state, feedUrl string) error {
+	fmt.Printf("Updating %v\n", feedUrl)
+
+	feed, err := s.db.GetFeedByUrl(context.Background(), feedUrl)
+	if err != nil {
+		return err
+	}
+
+	rssFeed, err := FetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return err
+	}
+
+	// Update feed's "last updated" timestamp
+	err = s.db.MarkFeedAsFetched(context.Background(), feed.ID)
+	if err != nil {
+		return fmt.Errorf("failed marking %v as updated", feedUrl)
+	}
+
+	saveFeedPosts(s, rssFeed, feed.ID)
+
+	return nil
+}
+
+// Save posts to the database
+func saveFeedPosts(s *state, rssFeed *RSSFeed, feedId uuid.UUID) error {
+	for i := range len(rssFeed.Channel.Item) {
+		pubDate, err := ParseRSSPubDate(rssFeed.Channel.Item[i].PubDate)
+		if err != nil {
+			return err
+		}
+
+		err = s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now(),
+			UpdatedAt:   time.Now(),
+			Title:       rssFeed.Channel.Item[i].Title,
+			Url:         rssFeed.Channel.Item[i].Link,
+			Description: rssFeed.Channel.Item[i].Description,
+			PublishedAt: pubDate,
+			FeedID:      feedId,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
