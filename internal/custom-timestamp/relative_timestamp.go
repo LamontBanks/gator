7ca156d49be69ce46cb4583e899b517bb0eb64cb
Lamont Banks <lamontbanks@gmail.com>
2025-04-27 11:43:19 -0400

    Unit tests

--- internal/custom-timestamp/relative_timestamp.go
+++ internal/custom-timestamp/relative_timestamp.go
@@ -1,7 +1,51 @@
 package customtimestamp
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func relativeTime(currDate time.Time, publishDate time.Time) string {
-	return currDate.Sub(publishDate).String()
+	timeSince := currDate.Sub(publishDate)
+
+	oneDay, err := time.ParseDuration("24h")
+	if err != nil {
+		return currDate.String()
+	}
+
+	// days
+	if timeSince >= oneDay {
+		numDays := int64(timeSince / (24 * time.Hour))
+		msgDays := appendMultiplesSuffixS("day", numDays)
+		return fmt.Sprintf("%v %v ago", numDays, msgDays)
+	}
+
+	// hours
+	if timeSince >= time.Hour && timeSince < oneDay {
+		numHours := int64(timeSince / time.Hour)
+		msgHours := appendMultiplesSuffixS("hour", numHours)
+		return fmt.Sprintf("%v %v ago", numHours, msgHours)
+	}
+
+	// minutes
+	if timeSince >= time.Minute && timeSince < time.Hour {
+		numMinutes := int64(timeSince / time.Minute)
+		msgMinutes := appendMultiplesSuffixS("minute", numMinutes)
+		return fmt.Sprintf("%v %v ago", numMinutes, msgMinutes)
+	}
+
+	// seconds
+	numSecond := int64(timeSince / time.Second)
+	msgSeconds := appendMultiplesSuffixS("second", numSecond)
+	return fmt.Sprintf("%v %v ago", numSecond, msgSeconds)
+}
+
+// Return `word` with appended "s" if count > 1
+// Otherwise return `word` unchanged
+func appendMultiplesSuffixS(word string, count int64) string {
+	if count > 1 {
+		word += "s"
+	}
+
+	return word
 }
--- internal/custom-timestamp/relative_timestamp_test.go
+++ internal/custom-timestamp/relative_timestamp_test.go
@@ -12,11 +12,63 @@ func TestSmoke(t *testing.T) {
 		timeElapsed     string
 		expected        string
 	}{
+		// Days
 		{
-			name:            "Basic",
+			name:            "1 day",
 			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
 			timeElapsed:     "24h",
 			expected:        "1 day ago",
+		},
+		{
+			name:            "More than 1, less than 2 days",
+			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
+			timeElapsed:     "36h",
+			expected:        "1 day ago",
+		},
+		{
+			name:            "2 days",
+			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
+			timeElapsed:     "48h",
+			expected:        "2 days ago",
+		},
+		// Hours
+		{
+			name:            "1 hour",
+			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
+			timeElapsed:     "1h",
+			expected:        "1 hour ago",
+		},
+		{
+			name:            "More than 1 hour, less than 1 day",
+			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
+			timeElapsed:     "11h",
+			expected:        "11 hours ago",
+		},
+		// Minutes
+		{
+			name:            "1 minute",
+			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
+			timeElapsed:     "1m",
+			expected:        "1 minute ago",
+		},
+		{
+			name:            "More than 1 minute, less than 1 hour",
+			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
+			timeElapsed:     "59m",
+			expected:        "59 minutes ago",
+		},
+		// Seconds
+		{
+			name:            "1 second",
+			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
+			timeElapsed:     "1s",
+			expected:        "1 second ago",
+		},
+		{
+			name:            "More than 1 second, less than 1 minute",
+			publishedAtDate: time.Date(2025, time.June, 15, 0, 0, 0, 0, time.UTC),
+			timeElapsed:     "59s",
+			expected:        "59 seconds ago",
 		},
 	}
 
@@ -33,7 +85,7 @@ func TestSmoke(t *testing.T) {
 
 		// Check
 		if actual != test.expected {
-			t.Errorf("\n%v\nActual: %v", test, actual)
+			t.Errorf("\nCase:\t\t\t%v\npublishedAtDate:\t%v\ntimeElapsed:\t\t%v\nExpected:\t\t%v\nActual:\t\t\t%v", test.name, test.publishedAtDate, test.timeElapsed, test.expected, actual)
 		}
 	}
 
